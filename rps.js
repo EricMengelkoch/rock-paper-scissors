f7725137c2334eb7e70b4ccef6576934d14a0bb4
Eric Mengelkoch <ericmengelkoch@gmail.com>
2024-07-12 18:57:05 -0500

    Created full game logic. Game is fully playable and functional

--- rps.js
+++ rps.js
@@ -24,19 +24,35 @@ function getHumanChoice() {
     }
 }
 
-function playRound(hmn, cpu) {
-    if (hmn == "Rock" && cpu == "Scissors" || hmn == "Paper" && cpu == "Rock" || hmn == "Scissors" && cpu == "Paper") {
-        console.log("You win! " + hmn + " beats " + cpu);
-        humanScore += 1;
-    } else if (cpu == "Rock" && hmn == "Scissors" || cpu == "Paper" && hmn == "Rock" || cpu == "Scissors" && hmn == "Paper") {
-        console.log("You lose! " + cpu + " beats " + hmn);
-        computerScore += 1;
+
+
+function playGame() {
+    let humanScore = 0;
+    let computerScore = 0;
+
+    function playRound(hmn, cpu) {
+        if (hmn == "Rock" && cpu == "Scissors" || hmn == "Paper" && cpu == "Rock" || hmn == "Scissors" && cpu == "Paper") {
+            console.log("You win! " + hmn + " beats " + cpu);
+            humanScore += 1;
+        } else if (cpu == "Rock" && hmn == "Scissors" || cpu == "Paper" && hmn == "Rock" || cpu == "Scissors" && hmn == "Paper") {
+            console.log("You lose! " + cpu + " beats " + hmn);
+            computerScore += 1;
+        } else {
+            console.log("It's a tie!")
+        }
+    }
+
+    while (humanScore < 3 && computerScore < 3) {
+        playRound(getHumanChoice(), getComputerChoice());
+    }
+
+    if (humanScore = 3) {
+        console.log("Congrats! You beat the computer " + humanScore + " to " + computerScore);
+    } else if (computerScore = 3) {
+        console.log("Yikes, beaten by the computer " + computerScore + " to " + humanScore);
     } else {
-        console.log("It's a tie!")
+        console.log("How did this game end??");
     }
 }
 
-let humanScore = 0;
-let computerScore = 0;
-
-playRound(getHumanChoice(), getComputerChoice());
+playGame();
