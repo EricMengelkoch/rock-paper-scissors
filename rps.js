7ff52142eff702d26b89c2f6873168b96e14faa3
Eric Mengelkoch <ericmengelkoch@gmail.com>
2024-07-12 18:49:22 -0500

    Completed round and score logic

--- rps.js
+++ rps.js
@@ -1,28 +1,42 @@
 function getComputerChoice() {
     let choice = Math.random() * 3;
     if (choice < 1) {
-        return "rock";
+        return "Rock";
     } else if (choice < 2) {
-        return "paper";
+        return "Paper";
     } else {
-        return "scissors";
+        return "Scissors";
     }
 }
 
 function getHumanChoice() {
-    let choice = prompt("rock, paper, or scissors?");
-    if (choice = "rock") {
-        return "rock";
-    } else if (choice = "paper") {
-        return "paper";
-    } else if (choice = "scissors") {
-        return "sciccors"
+    let first = prompt("Rock, Paper, or Scissors?");
+    let choice = first.toLowerCase();
+    if (choice == "rock") {
+        return "Rock";
+    } else if (choice == "paper") {
+        return "Paper";
+    } else if (choice == "scissors") {
+        return "Sciccors"
     } else {
         console.log("Invalid choice, try again");
         getHumanChoice();
     }
 }
 
-console.log(getComputerChoice());
+function playRound(hmn, cpu) {
+    if (hmn == "Rock" && cpu == "Scissors" || hmn == "Paper" && cpu == "Rock" || hmn == "Scissors" && cpu == "Paper") {
+        console.log("You win! " + hmn + " beats " + cpu);
+        humanScore += 1;
+    } else if (cpu == "Rock" && hmn == "Scissors" || cpu == "Paper" && hmn == "Rock" || cpu == "Scissors" && hmn == "Paper") {
+        console.log("You lose! " + cpu + " beats " + hmn);
+        computerScore += 1;
+    } else {
+        console.log("It's a tie!")
+    }
+}
 
-console.log(getHumanChoice());
+let humanScore = 0;
+let computerScore = 0;
+
+playRound(getHumanChoice(), getComputerChoice());
