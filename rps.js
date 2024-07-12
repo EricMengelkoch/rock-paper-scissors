901e84c695f6d567af35618a50747b1905d5f4db
Eric Mengelkoch <ericmengelkoch@gmail.com>
2024-07-12 18:32:37 -0500

    Added human choice logic

--- rps.js
+++ rps.js
@@ -9,4 +9,20 @@ function getComputerChoice() {
     }
 }
 
+function getHumanChoice() {
+    let choice = prompt("rock, paper, or scissors?");
+    if (choice = "rock") {
+        return "rock";
+    } else if (choice = "paper") {
+        return "paper";
+    } else if (choice = "scissors") {
+        return "sciccors"
+    } else {
+        console.log("Invalid choice, try again");
+        getHumanChoice();
+    }
+}
+
 console.log(getComputerChoice());
+
+console.log(getHumanChoice());
